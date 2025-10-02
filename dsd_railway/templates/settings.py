baa3ca87bef229e69efe874b0e4db5ac39b2030d
Eric Matthes <ehmatthes@gmail.com>
2025-10-02 16:08:41 -0400

    Add a Railway settings block.

--- dsd_railway/templates/settings.py
+++ dsd_railway/templates/settings.py
@@ -1,3 +1,33 @@
 {{current_settings}}
 
 # Railway settings.
+import os
+
+if os.environ.get("RAILWAY_PROJECT_NAME", ""):
+    # Configure a Postgres db.
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get("PGDATABASE", ""),
+            'USER': os.environ.get("PGUSER", ""),
+            'PASSWORD': os.environ.get("PGPASSWORD", ""),
+            'HOST': os.environ.get("PGHOST", ""),
+            'PORT': os.environ.get("PGPORT", ""),
+        }
+    }
+
+    # Helpful for development work.
+    print("\n\n***** ----- *****\n")
+    print(DATABASES)
+    print("\n***** ----- *****\n\n")
+
+    # Static files config.
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+    MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+    # Allowed hosts, CSRF.
+    ALLOWED_HOSTS = [".up.railway.app"]
+    CSRF_TRUSTED_ORIGINS = ['https://*.railway.app']
