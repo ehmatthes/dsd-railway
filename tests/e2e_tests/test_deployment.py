564472312f28bbfc2698e9e3796e7a1edbbe00f3
Eric Matthes <ehmatthes@gmail.com>
2025-10-06 21:39:07 -0400

    Get project id from env var for config-only and automate-all e2e tests.

--- tests/e2e_tests/test_deployment.py
+++ tests/e2e_tests/test_deployment.py
@@ -131,11 +131,19 @@ def test_deployment(tmp_project, cli_options, request):
     
     # Get URL and project ID from an automated deployment.
     if cli_options.automate_all:
+        # # Get project ID.
+        # cmd = "railway status --json"
+        # output = make_sp_call(cmd, capture_output=True).stdout.decode()
+        # output_json = json.loads(output)
+        # project_id = output_json["id"]
+        # request.config.cache.set("project_id", project_id)
+
+        breakpoint()
         # Get project ID.
-        cmd = "railway status --json"
+        cmd = f"railway variables --service {app_name} --json"
         output = make_sp_call(cmd, capture_output=True).stdout.decode()
         output_json = json.loads(output)
-        project_id = output_json["id"]
+        project_id = output_json["RAILWAY_PROJECT_ID"]
         request.config.cache.set("project_id", project_id)
 
         # Get URL
