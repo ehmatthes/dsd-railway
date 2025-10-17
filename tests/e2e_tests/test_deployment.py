4d7c7606049131e4a9fea8d04437979296ed273f
Eric Matthes <ehmatthes@gmail.com>
2025-10-17 17:49:08 -0400

    Pass as kw arg.

--- tests/e2e_tests/test_deployment.py
+++ tests/e2e_tests/test_deployment.py
@@ -36,7 +36,7 @@ def test_deployment(tmp_project, cli_options, request):
     python_cmd = it_utils.get_python_exe(tmp_project)
 
     # Run simple_deploy against the test project.
-    it_utils.run_simple_deploy(python_cmd, automate_all=cli_options.automate_all, cli_options.plugin_args_string)
+    it_utils.run_simple_deploy(python_cmd, automate_all=cli_options.automate_all, plugin_args_string=cli_options.plugin_args_string)
 
     # If testing Pipenv, lock after adding new packages.
     if cli_options.pkg_manager == "pipenv":
