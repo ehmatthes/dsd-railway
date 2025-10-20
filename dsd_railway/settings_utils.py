f00cd58499ad250b301b11fc1e0b1de85bd1bacf
Eric Matthes <ehmatthes@gmail.com>
2025-10-20 17:24:04 -0400

    clean up diagnostics.

--- dsd_railway/settings_utils.py
+++ dsd_railway/settings_utils.py
@@ -6,15 +6,24 @@ from django_simple_deploy.management.commands.utils.plugin_utils import dsd_conf
 
 
 def get_db_block(templates_path, db):
-    """Get DATABASES settings block."""
+    """Get DATABASES settings block.
+    
+    Uses mark_safe(), so block is not escaped by template engine.
+    """
     # No need for a conditional block, use --db arg to get correct template
     # for DATABASES block.
     path_db_block = templates_path / f"db_block_{db}.py"
     db_block = path_db_block.read_text()
 
-    if not dsd_config.settings_path.parts[-2:] == ("settings", "production.py"):
-        # Non-wagtail projects need an indented settings block.
-        db_block = db_block.replace("\n", "\n    ")
+    if dsd_config.wagtail_project:
+        # Wagtail projects don't need indentation.
+        return mark_safe(db_block)
 
-    # Mark safe, so block is not escaped by template engine.
-    return mark_safe(db_block)
+    # Non-wagtail projects need an indented settings block.
+    # Get lines, indent them all, then remove indentation from first line.
+    lines = db_block.splitlines()
+    indented_lines = [f"    {l}" if l else l for l in lines]
+    indented_block = "\n".join(indented_lines)
+    indented_block = indented_block.removeprefix("    ")
+
+    return mark_safe(indented_block)
--- tests/integration_tests/reference_files/settings_sqlite.py
+++ tests/integration_tests/reference_files/settings_sqlite.py
@@ -144,7 +144,7 @@ if os.environ.get("RAILWAY_PROJECT_NAME", ""):
     # You can override individual settings in the call to `sqlite_config()`.
     # Example: `sqlite_config(BASE_DIR, file_name="production_db.sqlite3")``
     from dj_lite import sqlite_config
-    
+
     path_data = Path("data")
     DATABASES = {
         "default": sqlite_config(base_dir=path_data),
